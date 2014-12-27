afd2048340a4d5c6b465eb43719878a9a2dd4114
Mat Utter <matutter4@gmail.com>
2014-12-27 17:02:09 -0500

    webrelay windows fox

--- webRelay/server/getIP.js
+++ webRelay/server/getIP.js
@@ -1,23 +1,22 @@
 var os=require('os');
 var ifaces=os.networkInterfaces();
 
-function get( face ) { /* heheh */
-	face = face || 'eth1'
-	for (var dev in ifaces) {
-	  var alias=0;
-	  ifaces[dev].forEach(function(details){
-	    if (details.family=='IPv4') {
-	      if( face == dev ) {
-	      	face = details.address;
-	      	return;
-	      }
-	      ++alias;
+function get( s ) {
+	var interfaces = os.networkInterfaces()
+	  , addresses = []
+	  , ret = 'none';
+	for (var k in interfaces) {
+	    for (var k2 in interfaces[k]) {
+	        var address = interfaces[k][k2];
+	        console.log( " > " + k )
+	        if (address.family === 'IPv4' && !address.internal) {
+	            addresses.push(address.address);
+	        }
 	    }
-	  });
-	}	
-	return face;	
+	}
+	return addresses[1];
 }
 
-
+//console.log( get() )
 
 exports.get = get;
